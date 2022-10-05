a600ff31cce68b1718be1daa736960ac0d569bc5
a-tibi <ahmd_tibi@yahoo.com>
2022-10-05 10:57:24 +0400

    add codes

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queue2Stacks
+{
+	class Program
+	{
+		public static Stack<int> input = new Stack<int>();
+		public static Stack<int> output = new Stack<int>();
+
+		static void Enqueue(int item)
+		{
+			input.Push(item); 
+		}
+
+		static int Dequeue()
+		{
+			InputToOutput();
+
+			return output.Pop();
+		}
+
+		static int Print()
+		{
+			InputToOutput();
+
+			return output.Peek();
+		}
+
+		static void InputToOutput()
+		{
+			while (input.Count != 0)
+			{
+				output.Push(input.Pop());
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			int queries = int.Parse(Console.ReadLine());
+
+			while (queries > 0)
+			{
+				string[] lines = Console.ReadLine().Split(' ');
+
+				int command = int.Parse(lines[0]);
+
+				switch (command)
+				{
+					case 1:
+						Enqueue(int.Parse(lines[1]));
+						break;
+					case 2:
+						Dequeue();
+						break;
+					case 3:
+						Console.WriteLine(Print());
+						break;
+					default:
+						break;
+				}
+
+				queries--;
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
